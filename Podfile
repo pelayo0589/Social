070dd6cd03bb75f9e740666c8fcc4952840aa57f
Pelayo Mercado <pelayo@Pelayos-MacBook-Pro.local>
2017-07-23 07:01:04 -0400

    basic sign in

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Social' do
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+pod 'Firebase/Storage'
+
+
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Social
+
+end
---
+++ Social/SignInVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Pelayo Mercado on 7/22/17.
+//  Copyright © 2017 Pelayo Mercado. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
